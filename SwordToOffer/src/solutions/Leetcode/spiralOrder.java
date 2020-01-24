53bac68af7951326728f8120ae1445e226d78076
invsc <864645506@qq.com>
2020-01-24 18:45:59 +0800

    change
    complete  spiralOrder

--- SwordToOffer/src/solutions/Leetcode/spiralOrder.java
+++ SwordToOffer/src/solutions/Leetcode/spiralOrder.java
@@ -12,43 +12,70 @@ import java.util.List;
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> res = new ArrayList<>();
-        int height = matrix.length;
-        int width = matrix[0].length;
-        for ()
+        if (matrix.length == 0) {
+            return res;
+        }
+        int height1 = 0;
+        int width1 = 0;
+        int height2 = matrix.length;
+        int width2 = matrix[0].length;
+        while (true) {
+            res.addAll(easyOrder(matrix, height1, height2, width1, width2));
+            height1++;
+            width1++;
+            height2--;
+            width2--;
+            if (height1 >= height2 || width1 >= width2) break;
+        }
         return res;
     }
 
-    public List<Integer> easyOrder(int[][] matrix, int height, int width) {
+    public List<Integer> easyOrder(int[][] matrix, int height1, int height2, int width1, int width2) {
         List<Integer> res = new ArrayList<>();
         int i, j; // i为行号, j为列号
         // 保存上边框的数字
-        i = 0;
-        for (j = 0; j < matrix[0].length; j++) {
+        i = height1;
+        for (j = width1; j < width2; j++) {
             res.add(matrix[i][j]);
         }
         // 保存右边框的数字
-        j = matrix.length - 1;
-        for (i = 1; i < matrix.length; i++) {
+        j = width2 - 1;
+        for (i = height1 + 1; i < height2; i++) {
             res.add(matrix[i][j]);
         }
         // 保存下边框的数字
-        i = matrix.length - 1;
-        for (j = matrix[0].length - 2; j > -1; j--) {
-            res.add(matrix[i][j]);
+        i = height2 - 1;
+        if (i != height1) {
+            for (j = width2 - 2; j > width1 - 1; j--) {
+                res.add(matrix[i][j]);
+            }
         }
         // 保存左边框的数字
-        j = 0;
-        for (i = matrix.length - 2; i > 0; i--) {
-            res.add(matrix[i][j]);
+        j = width1;
+        if (j != width2 - 1) {
+            for (i = height2 - 2; i > height1; i--) {
+                res.add(matrix[i][j]);
+            }
         }
+
         return res;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[][] matrix = {{1, 2, 3},
-                {4, 5, 6},
-                {7, 8, 9}};
-        System.out.println(solution.easyOrder(matrix));
+        int[][] matrix = {
+                {1,  2,  3},
+                {4,  5,  6},
+                {7,  8,  9}};
+        int[][] matrix2 = {{1}};
+        int[][] matrix3 = {
+                {1, 2, 3, 4},
+                {5,  6, 7, 8},
+                {9, 10, 11, 12}};
+        int[][] matrix4 = {
+                {1},
+                {2}
+        };
+        System.out.println(solution.spiralOrder(matrix));
     }
 }
