3368d37b4082e3eae7f0b28e2c0fe96d2be727a5
invsc <864645506@qq.com>
2020-02-12 19:00:38 +0800

    syns

--- SwordToOffer/src/solutions/Leetcode/NodeCopyRandomListSolution.java
+++ SwordToOffer/src/solutions/Leetcode/NodeCopyRandomListSolution.java
@@ -5,10 +5,8 @@ package solutions.Leetcode;
  * https://leetcode-cn.com/problems/copy-list-with-random-pointer/
  */
 
-import javax.xml.soap.Node;
-
 /**
-// Definition for a Node.
+Definition for a Node.
 class Node {
     int val;
     Node next;
@@ -38,14 +36,32 @@ class NodeCopyRandomListSolution {
         if (head == null) {
             return null;
         }
-        Node oldNode = head;
-        Node newNode = new Node(oldNode.val);
+        // 首先考虑没有随机指针的结构
+        Node newHead = new Node(head.val); // 新的节点头
+        // 依次遍历原链表的每个节点
+        Node newNode = newHead; // 用于遍历的新节点
+        Node oldNode = head; // 用于遍历的原节点
         while (oldNode.next != null) {
             oldNode = oldNode.next;
             newNode.next = new Node(oldNode.val);
             newNode = newNode.next;
         }
-        return newNode;
+        // 处理添加随机指针的情况
+        oldNode = head; // 用于遍历的原节点
+        newNode = newHead; // 用于遍历的新节点
+        if (oldNode.random != null) {
+            int index = 0; // 待求的random对应index
+            Node currentNode = head;
+            while (currentNode.next != null) {
+                index++;
+            }
+        }
+        while (oldNode.next != null) {
+            oldNode = oldNode.next;
+            newNode.next = new Node(oldNode.val);
+            newNode = newNode.next;
+        }
+        return newHead;
     }
 }
 
