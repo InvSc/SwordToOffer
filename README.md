78bc9d00e2da8d9a303ef716a5a86764334d67c6
invsc <864645506@qq.com>
2020-02-15 17:21:23 +0800

    small changes

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # SwordToOffer
+
+# 跳过的题目：
+- 从上往下打印二叉树
+https://leetcode-cn.com/problems/binary-tree-vertical-order-traversal/
+- 二叉搜索树的后序遍历序列
+leetcode无原题
--- SwordToOffer/src/solutions/Leetcode/NodeCopyRandomListSolution.java
+++ SwordToOffer/src/solutions/Leetcode/NodeCopyRandomListSolution.java
@@ -4,9 +4,6 @@ package solutions.Leetcode;
  * LeetCode 138. 复制带随机指针的链表
  * https://leetcode-cn.com/problems/copy-list-with-random-pointer/
  */
-
-import javax.xml.soap.Node;
-
 /**
 // Definition for a Node.
 class Node {
@@ -38,14 +35,35 @@ class NodeCopyRandomListSolution {
         if (head == null) {
             return null;
         }
-        Node oldNode = head;
-        Node newNode = new Node(oldNode.val);
+        // 首先考虑没有随机指针的结构
+        Node newHead = new Node(head.val); // 新的节点头
+        // 依次遍历原链表的每个节点
+        Node newNode = newHead; // 用于遍历的新节点
+        Node oldNode = head; // 用于遍历的原节点
         while (oldNode.next != null) {
             oldNode = oldNode.next;
             newNode.next = new Node(oldNode.val);
             newNode = newNode.next;
         }
-        return newNode;
+        // 添加随机指针
+        oldNode = head; // 用于遍历的原节点
+        newNode = newHead; // 用于遍历的新节点
+        if (oldNode.random != null) {
+            int index = 0; // 待求的random值
+            Node currentNode = head;
+            while (currentNode.next != null) {
+                index++;
+            }
+        }
+        while (oldNode.next != null) {
+            oldNode = oldNode.next;
+            newNode.next = new Node(oldNode.val);
+            newNode = newNode.next;
+        }
+        return newHead;
+    }
+    public int computeIndex(Node head, ) {
+
     }
 }
 
